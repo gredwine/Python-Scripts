084603273224ff577043f777ad7fafe2e7237297
Gerry <gredwine@runbox.com>
2015-03-23 13:46:29 -0400

    First checkin

---
+++ appendsum.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+def appendsum(lst):
+
+	for c in range(25):
+		last3 = lst[-3:]
+		lst.append(sum(last3))
+
+	return lst
+
+
+mylist = [0,1,2]
+
+x = appendsum(mylist)
+
+print x[10], x[20]
